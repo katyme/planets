1f266b0598e465e388544278d796e4bc4550720d
Katy Melendez <kmfontanez@gmail.com>
2020-07-28 17:33:06 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    
+    <title>Planets</title>
+
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1>The Planets in Our Solar System</h1>
+                <div class="table-responsive">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th>Image</th>
+                                <th>Planet</th>
+                                <th>Google Maps</th>
+                                <th>Description</th>
+                                <th>Distance from Sun</th>
+                                <th>Radius</th>
+                                <th>Mass</th>
+                                <th>Length of Day</th>
+                                <th>Orbital Period</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <img src="images/mercury.jpg">
+                                </td>
+                                <td>Mercury</td>
+                                <td>
+                                    <a href="http://google.com/maps/space/mercury" target="_blank" class="btn btn-info">View</a>
+                                </td>
+                                <td>Mercury is the smallest and innermost planet in the Solar System. Its orbit around the Sun takes 87.97 days, the shortest of all the planets in the Solar System. It is named after the Roman deity Mercury, the messenger of the gods.</td>
+                                <td>35.98 million miles</td>
+                                <td>1,516 miles</td>
+                                <td>3.285 × 10^23 kg</td>
+                                <td>58d 15h 30m</td>
+                                <td>88 days</td>
+                            </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+</body>
+</html>
